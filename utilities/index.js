284bfb9d6f47fee4bc825f831b1eade5d8ab7d6b
emmanuel2011 <okoyee61@gmail.com>
2025-04-02 04:27:51 +0100

    building a single listing of items for week 3

--- utilities/index.js
+++ utilities/index.js
@@ -56,4 +56,88 @@ Util.buildClassificationGrid = async function(data){
   return grid
 }
 
+/**
+ * Building a single listing element from data allowing vehicles to be displayed 
+ */
+Util.buildItemListing = async function (data) {
+  let listingHTML = "";
+  console.dir({ data });
+  if (data) {
+    listingHTML = `
+      <section class="car-listing">
+        <img src="${data.inv_image}" alt="${data.inv_make} ${data.inv_model}">
+        <div class="car-information">
+          <div>
+            <h2>${data.inv_year} ${data.inv_make} ${data.inv_model}</h2>
+          </div>
+          <div>
+            ${Number.parseFloat(data.inv_price).toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}
+          </div>
+          <div class="description">
+            <p>
+              ${data.inv_description}
+            </p>
+            <dl>
+              <dt>MILEAGE</dt>
+              <dd>${data.inv_miles.toLocaleString("en-US", {
+                style: "decimal",
+              })}</dd>
+              <dt>COLOR</dt>
+              <dd>${data.inv_color}</dd>
+              <dt>CLASS</dt>
+              <dd>${data.classification_name}</dd>
+            </dl>
+          </div>
+
+        </div>
+      </section>
+    `;
+    // listingHTML += '<img src="/images/notexist.jpg">'; // Introduce 404 error
+  } else {
+    listingHTML = `
+      <p>Sorry, no matching vehicles could be found.</p>
+    `;
+  }
+  return listingHTML;
+};
+
+/**
+ * Build an HTML select element with classification data
+ * @param {int} classification_id
+ * @returns {string}
+ */
+
+Util.buildClassificationList = async function (classification_id = null) {
+  let data = await invModel.getClassifications();
+  let classificationList =
+    '<select name="classification_id" id="classificationList" required>';
+  classificationList += "<option value=''>Choose a Classification</option>";
+  data.rows.forEach((row) => {
+    classificationList += '<option value="' + row.classification_id + '"';
+    if (
+      classification_id != null &&
+      row.classification_id == classification_id
+    ) {
+      classificationList += " selected ";
+    }
+    classificationList += ">" + row.classification_name + "</option>";
+  });
+  classificationList += "</select>";
+  return classificationList;
+};
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+
+
+
+
+
+
 module.exports = Util
